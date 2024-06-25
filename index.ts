12535efbc6f5e4d11464ceaa850f2722a6375683
sharno <sharnoby3@gmail.com>
2024-06-25 11:53:19 -0400

    Add dotenv and use v1 api for not being rate limited

--- index.ts
+++ index.ts
@@ -3,94 +3,116 @@ import { join } from "node:path";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { readFileSync } from "node:fs";
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-const appKey = "FILL_YOUR_KEY";
-const appSecret = "FILL_YOUR_KEY";
-const accessToken = "FILL_YOUR_KEY";
-const accessSecret = "FILL_YOUR_KEY";
+const appKey = process.env.appKey;
+const appSecret = process.env.appSecret;
+const accessToken = process.env.accessToken;
+const accessSecret = process.env.accessSecret;
 
-const argv = await yargs(hideBin(process.argv))
-	.usage("Usage: $0 --path <path> [--from <date>] [--until <date>]")
-	.option("path", {
-		alias: "p",
-		describe: "Path to the folder to read",
-		type: "string",
-		demandOption: true,
-	})
-	.option("from", {
-		describe: "Start date for filtering (YYYY-MM-DD)",
-		type: "string",
-		demandOption: true,
-	})
-	.option("until", {
-		describe: "End date for filtering (YYYY-MM-DD)",
-		type: "string",
-		demandOption: true,
-	})
-	.help("h")
-	.alias("h", "help").argv;
-
-const path = join(argv.path, "data", "tweets.js");
-console.log(path);
-
-let text = readFileSync(path, "utf8");
-text = text.slice(text.indexOf("["));
-
-interface Tweet {
-	tweet: {
-		edit_info: {
-			initial: {
-				editableUntil: string;
-				editTweetIds: string[];
-			};
-		};
-	};
-}
-const json: Tweet[] = JSON.parse(await text);
-
-const toDel = json
-	.map((t) => t.tweet.edit_info.initial)
-	.filter((t) => t.editableUntil >= argv.from && t.editableUntil <= argv.until)
-	.toSorted((a, b) => a.editableUntil.localeCompare(b.editableUntil));
-
-const tweetIds = toDel.map((t) => t.editTweetIds[0]);
-
-// Connect to Twitter and delete
-const twitterClient = new TwitterApi({
-	appKey,
-	appSecret,
-	accessToken,
-	accessSecret,
-});
-
-async function deleteTweet(tweetId: string) {
-	console.log("deleting", tweetId);
-	const res = await twitterClient.v2
-		.deleteTweet(tweetId)
-		.catch((e) => console.error(e));
-	if (res) {
-		console.log(res);
+(async () => {
+	if (!appKey || !appSecret || !accessToken || !accessSecret) {
+		console.error(
+			"you need to add a .env file with the required keys: appKey, appSecret, accessToken, accessSecret",
+		);
+		return;
 	}
-}
-const delTasks = tweetIds.map((id) => () => deleteTweet(id));
 
-const processInPool = async (
-	tasks: (() => Promise<unknown>)[],
-	poolSize: number,
-) => {
-	const pool = tasks.toReversed();
-	const worker = async () => {
-		while (true) {
-			const task = pool.pop();
-			if (task == null) {
-				return;
-			}
-			await task();
+	const argv = await yargs(hideBin(process.argv))
+		.usage("Usage: $0 --path <path> [--from <date>] [--until <date>]")
+		.option("path", {
+			alias: "p",
+			describe: "Path to the folder to read",
+			type: "string",
+			demandOption: true,
+		})
+		.option("from", {
+			describe: "Start date for filtering (YYYY-MM-DD)",
+			type: "string",
+			demandOption: true,
+		})
+		.option("until", {
+			describe: "End date for filtering (YYYY-MM-DD)",
+			type: "string",
+			demandOption: true,
+		})
+		.help("h")
+		.alias("h", "help").argv;
+
+	const path = join(argv.path, "data", "tweets.js");
+	console.log(path);
+
+	let text = readFileSync(path, "utf8");
+	text = text.slice(text.indexOf("["));
+
+	interface Tweet {
+		tweet: {
+			edit_info: {
+				initial: {
+					editableUntil: string;
+					editTweetIds: string[];
+				};
+			};
+			full_text: string;
+		};
+	}
+
+	interface MyTweet {
+		id: string;
+		date: string;
+		content: string;
+	}
+	const json: Tweet[] = JSON.parse(await text);
+
+	const toDel = json
+		.map<MyTweet>((t) => ({
+			id: t.tweet.edit_info.initial.editTweetIds[0],
+			date: t.tweet.edit_info.initial.editableUntil,
+			content: t.tweet.full_text,
+		}))
+		.filter((t) => t.date >= argv.from && t.date <= argv.until)
+		.toSorted((a, b) => a.date.localeCompare(b.date));
+
+	// Connect to Twitter and delete
+	const twitterClient = new TwitterApi({
+		appKey,
+		appSecret,
+		accessToken,
+		accessSecret,
+	});
+
+	async function deleteTweet(tweet: MyTweet) {
+		const res = await twitterClient.v1
+			.deleteTweet(tweet.id)
+			.catch((e) => console.error(e));
+		console.log("deleted:", tweet);
+		if (res) {
+			console.log("response:", "success");
+		} else {
+			console.error("response:", "failed");
 		}
+	}
+	const delTasks = toDel.map((t) => () => deleteTweet(t));
+
+	const processInPool = async (
+		tasks: (() => Promise<unknown>)[],
+		poolSize: number,
+	) => {
+		const pool = tasks.toReversed();
+		const worker = async () => {
+			while (true) {
+				const task = pool.pop();
+				if (task == null) {
+					return;
+				}
+				await task();
+			}
+		};
+
+		const workers = Array.from({ length: poolSize }, () => worker());
+		return Promise.allSettled(workers);
 	};
 
-	const workers = Array.from({ length: poolSize }, () => worker());
-	return Promise.allSettled(workers);
-};
-
-await processInPool(delTasks, 2);
+	await processInPool(delTasks, 5);
+})();
